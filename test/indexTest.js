04bf669a58f7cd160c2ac3736eb224a3140aefe7
Aisha hassan diini <183903634+ashadini@users.noreply.github.com>
2024-10-12 15:42:47 +0300

    created my in html

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,15 +1,13 @@
-const { expect } = require("chai");
-const fs = require("fs");
-const path = require("path");
+const assert = require('assert');
+const fs = require('fs');
 
-describe("my_list.html", () => {
-  it("should be present", () => {
-    const fileExists = fs.existsSync(
-      path.resolve(__dirname, "..", "my_list.html")
-    );
-    const errorHint =
-      "Make sure you created a file called 'my_list.html' in the lab directory with your HTML document in it!";
-
-    expect(fileExists, errorHint).to.be.true;
-  });
+describe('HTML File', function() {
+    it('should have a title', function(done) {
+        fs.readFile('my_list.html', 'utf8', function(err, data) {
+            if (err) throw err;
+            assert(data.includes('<title>My Plant List</title>'));
+            done();
+        });
+    });
+    // Add more test cases as needed
 });
